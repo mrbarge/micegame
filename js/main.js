b82f77c53e83a056a3937ef9fc80dcc778b64e95
mrbarge <mrbarge@user.noreply.github.com>
2025-09-16 11:31:35 +0000

    Automated minify of 46f40073d1ae3aee53ca9e729c319f61c61ea6d3

--- js/main.js
+++ js/main.js
@@ -1,18 +1,1 @@
-import { GAME_CONFIG } from './gameConfig.js';
-import { GameScene } from './gameScene.js';
-
-// Initialize the game
-const config = {
-    type: Phaser.AUTO,
-    width: GAME_CONFIG.GRID_WIDTH * GAME_CONFIG.CELL_SIZE + 70,
-    height: GAME_CONFIG.GRID_HEIGHT * GAME_CONFIG.CELL_SIZE + 140,
-    parent: 'phaser-game',
-    backgroundColor: '#2c3e50',
-    scene: GameScene,
-    scale: {
-        mode: Phaser.Scale.NONE, // Disable auto-scaling
-        autoCenter: Phaser.Scale.NO_CENTER // Disable auto-centering
-    }
-};
-
-const game = new Phaser.Game(config);
+import{GAME_CONFIG}from"./gameConfig.js";import{GameScene}from"./gameScene.js";const config={type:Phaser.AUTO,width:GAME_CONFIG.GRID_WIDTH*GAME_CONFIG.CELL_SIZE+70,height:GAME_CONFIG.GRID_HEIGHT*GAME_CONFIG.CELL_SIZE+140,parent:"phaser-game",backgroundColor:"#2c3e50",scene:GameScene,scale:{mode:Phaser.Scale.NONE,autoCenter:Phaser.Scale.NO_CENTER}},game=new Phaser.Game(config);
